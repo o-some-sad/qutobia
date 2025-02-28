b1b79c37b346d57a6623fb622f3172e2c1cadd72
JanaHazem12 <jana.hazem.abbass@gmail.com>
2025-03-01 00:59:05 +0200

    Rename files for better readability

---
+++ packages/backend/routes/book.route.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+// let formData = new FormData();
+// formData.append('username', 'john_doe');
+// formData.append('profilePic', document.querySelector('input[type="file"]').files[0]);
+
+// router.fetch('/upload', {
+//             method: 'POST',
+//             body: formData
+//         }
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error))
+// );
+
+
+
+
+
+// router.get(); // get ALL or get by filters
+// router.get(); // get by ID
+// router.patch(); // modify a book's details
+// router.delete(); // delete a book
