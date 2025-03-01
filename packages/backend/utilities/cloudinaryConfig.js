423dd16006b28159252ab86442af3dc578c59c98
A7med zaki <ahmedzaki@gmail.com>
2025-03-01 17:29:36 +0200

    edit cloudinary config

---
+++ packages/backend/utilities/cloudinaryConfig.js
@@ -0,0 +1,41 @@
+import multer from 'multer';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true
+});
+
+const userStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'kotobia/users',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+    public_id: (req) => `user_${req.params.id}`,
+    overwrite: true
+  }
+});
+
+const bookStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'kotobia/books',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+    overwrite: true
+  }
+});
+
+export const uploadUserImage = multer({
+  storage: userStorage,
+  limits: { fileSize: 1024 * 1024 }, // 1 MB
+});
+export const uploadBookImage = multer({
+  storage: bookStorage,
+  limits: { fileSize: 1024 * 1024 }, // 1 MB
+});
+export { cloudinary };
