9bca3a4ace60f3a79d8e4ee101e891a1f74b510d
OIsmail99 <osama.m.esmael999@gmail.com>
2025-03-01 22:37:53 +0200

    add env template

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,8 +1,8 @@
-import process from 'node:process';
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import appRouter from './routes/index.js';
+import process from "node:process";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import appRouter from "./routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log('Mongoose connection successfully established');
+    console.log("Mongoose connection successfully established");
   } catch (err) {
     console.error(`Mongoose connection error:${err}`);
     process.exit(1);
@@ -18,20 +18,22 @@ const connectDB = async () => {
 };
 
 app.get("/", (req, res) => {
-  res.end(`<div style="text-align: center;"><h1>Welcome to Kotobia WebSite</h1></div>`);
+  res.end(
+    `<div style="text-align: center;"><h1>Welcome to Kotobia WebSite</h1></div>`
+  );
 });
 
 app.get("/api/hello", (req, res) => {
   res.json({
-    ok: true
+    ok: true,
   });
 });
 
 app.use(express.json());
-app.use('/api', appRouter);
+app.use("/api", appRouter);
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
-    message: err.message
+    message: err.message,
   });
 });
 
