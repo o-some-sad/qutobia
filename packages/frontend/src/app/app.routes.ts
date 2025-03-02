bc0eabb8e6ee433837aaf772dea6c7a839c7dccb
ilawy <next.mohammed.amr@gmail.com>
2025-03-02 10:38:36 +0200

    feat: add styling foundation for frontend

--- packages/frontend/src/app/app.routes.ts
+++ packages/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { isDevMode } from '@angular/core';
 import { Routes } from '@angular/router';
+import { PreviewComponent } from './routes/preview/preview.component';
 
 export const routes: Routes = [];
+
+
+if(isDevMode()){
+    routes.push({
+        path: 'preview',
+        component: PreviewComponent
+    })
+}
