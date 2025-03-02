26df1b170014cb87ed52000c14a56d30d3b57f0e
ilawy <next.mohammed.amr@gmail.com>
2025-03-02 14:18:22 +0000

    feat(frontend): add dummy search bar

--- packages/frontend/src/app/components/header/header.component.ts
+++ packages/frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  searchValue = signal('')
+  searchFocused = signal(false)
+  searchResults = signal<string[]>([])
 
+  constructor() {
+    let ival: number | undefined = undefined;
+    effect(() => {
+      clearTimeout(ival)
+      if (!this.searchFocused()) return;
+      if (!this.searchValue()) {
+        this.searchResults.set([])
+        return
+      }
+      ival = setTimeout(() => {
+        console.log(`The current count is: ${this.searchValue()}`);
+        this.searchResults.set(Array.from({ length: 5 }).map(() => Math.random().toString()))
+      }, 1000);
+    });
+  }
 }
