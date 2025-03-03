91962e1e139b7a10ba34b1744a4759f6617e8b6f
ilawy <next.mohammed.amr@gmail.com>
2025-03-03 23:21:31 +0000

    fix: user middleware won't proceed to the next middleware

--- packages/backend/middlewares/auth.middleware.js
+++ packages/backend/middlewares/auth.middleware.js
@@ -5,8 +5,9 @@ import User from "../models/user.model"
 
 export function authMiddleware(adminOnly = false){
     /**@type {import("express").Handler} */
-    return (req, res, next)=>{
-        const user = User.findOne()
+    return async (req, res, next)=>{
+        const user = await User.findOne()
         req.user = user
+        next()
     }
 }
