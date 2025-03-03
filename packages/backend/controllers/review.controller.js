1829ac46163fd3e15cce2b34cc12f89e6cd365e2
OIsmail99 <osama.m.esmael999@gmail.com>
2025-03-03 07:55:51 +0200

    update review controller & router

--- packages/backend/controllers/review.controller.js
+++ packages/backend/controllers/review.controller.js
@@ -1,16 +1,13 @@
 //@ts-check
+import mongoose from "mongoose";
 import Review from "../models/review.model.js";
+import express from "express";
 export const addReview = async (req, res, next) => {
   try {
     const reviewData = { ...req.body };
     const review = await Review.create(reviewData);
     return res.status(200).json(review);
   } catch (err) {
-    if (err.code === 11000) {
-      return res.status(400).json({
-        message: "You can only write one review per book.",
-      });
-    }
     next(err);
   }
 };
@@ -18,11 +15,61 @@ export const addReview = async (req, res, next) => {
 export const getAllReviewsByBookId = async (req, res, next) => {
   try {
     const bookId = req.params.id;
-    const reviews = await Review.find({ bookId });
+    if (!mongoose.Types.ObjectId.isValid(bookId)) {
+      throw new Error("not a valid book ID");
+    }
+    const reviews = await Review.find({ book: bookId })
+      .populate("user", "name")
+      .populate("book", "title")
+      .exec();
+    if (reviews.length === 0) {
+      throw new Error("review not found");
+    }
     return res.status(200).json(reviews);
   } catch (err) {
     next(err);
   }
 };
-// export const updateReview = (req, res) => {};
-// export const deleteReview = (req, res) => {};
+
+export const updateReviewById = async (req, res, next) => {
+  //authorization still nedded
+  //any extra fields will be accepted but not added to the database.
+  try {
+    const reviewId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+      throw new Error("review Id is invalid");
+    }
+    const updatedFields = req.body;
+    const updatedReview = await Review.findByIdAndUpdate(
+      reviewId,
+      { ...updatedFields },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedReview) {
+      throw new Error("review doesn't exist");
+    }
+    res.status(200).json(updatedReview);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteReviewById = async (req, res, next) => {
+  //authorization needed
+  try {
+    const reviewId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+      throw new Error("invalid review id");
+    }
+    const deleted = await Review.deleteOne({ _id: reviewId });
+    if (deleted.deletedCount === 0) {
+      throw new Error("review not found");
+    }
+    res.status(200).json({ message: "deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
--- packages/backend/routes/review.route.js
+++ packages/backend/routes/review.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 // import functionality from controller, MiddleWares should be added in the middle
-import { addReview } from "../controllers/review.controller.js";
+import {
+  addReview,
+  getAllReviewsByBookId,
+  updateReviewById,
+  deleteReviewById,
+} from "../controllers/review.controller.js";
 const Router = express.Router();
 Router.post("/", addReview);
-// Router.get("/:id", getAllReviewsByBookId);
-// Router.patch("/:id", updateReview);
-// Router.delete("/:id", deleteReview);
+Router.get("/:id", getAllReviewsByBookId);
+Router.patch("/:id", updateReviewById);
+Router.delete("/:id", deleteReviewById);
 
 export default Router;
