e6ac1a725d0096890ae4508dea056d9c417890fd
ilawy <next.mohammed.amr@gmail.com>
2025-03-03 13:37:42 +0000

    add new index route

--- packages/frontend/src/app/app.routes.ts
+++ packages/frontend/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { isDevMode } from '@angular/core';
 import { Routes } from '@angular/router';
 import { PreviewComponent } from './routes/preview/preview.component';
+import { IndexComponent } from './routes/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent,
+        title: "Kotobia - Home"
+    }
+];
 
 
 if(isDevMode() || true){
