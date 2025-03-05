4b12d96e9fd094a9b32f528d3f8be148e2b7f7e8
ilawy <next.mohammed.amr@gmail.com>
2025-03-05 22:19:04 +0200

    fix: duplicate import because of merge

--- packages/backend/routes/index.js
+++ packages/backend/routes/index.js
@@ -1,10 +1,10 @@
+//@ts-check
 import express from 'express';
 import bookRouter from './book.route.js';
 import userRouter from './user.route.js';
 import reviewRouter from "./review.route.js";
 import orderRouter from './order.route.js'
 import cartRouter from './cart.route.js';
-import orderRouter from './order.route.js';
 import adminRouter from './admin.route.js'
 
 const Router = express.Router();
@@ -14,6 +14,6 @@ Router.use('/books', bookRouter);
 Router.use('/users', userRouter);
 Router.use('/reviews', reviewRouter);
 Router.use('/cart', cartRouter)
-Router.all('*', (req, res) => res.status(404).json({status: 'fail', message: 'Route not found'}));
+Router.all('*', (req, res) => {res.status(404).json({status: 'fail', message: 'Route not found'})});
 
 export default Router;
