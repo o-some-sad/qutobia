268451fdf46ad93196b306c3f8be21f9be6cd23f
OIsmail99 <osama.m.esmael999@gmail.com>
2025-03-05 10:40:59 +0200

    add cors & dummy logger

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import appRouter from "./routes/index.js";
+import cors from "cors";
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware.js";
 
 dotenv.config();
@@ -17,6 +18,25 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+const whiteList = [`http://localhost:${process.env.PORT || 3000}`]; //frontend to be added
+const corsOptions = {
+  origin: (originWeb, callback) => {
+    if (!originWeb || whiteList.includes(originWeb)) {
+      callback(null, true);
+    } else {
+      callback(new Error("not allowed"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  //dummy logger for testing
+  console.log(`${req.method} ..  ${req.url}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.end(
@@ -36,5 +56,5 @@ app.use(handleErrorMiddleware);
 
 app.listen(process.env.PORT, async () => {
   await connectDB();
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+  console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
 });
