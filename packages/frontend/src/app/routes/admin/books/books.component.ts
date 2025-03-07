b719b934cce6b1a7ea0022d657b84462cb93c803
ilawy <next.mohammed.amr@gmail.com>
2025-03-07 22:08:53 +0200

    feat: books table

--- packages/frontend/src/app/routes/admin/books/books.component.ts
+++ packages/frontend/src/app/routes/admin/books/books.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { DomPortal, Portal } from  '@angular/cdk/portal'
 import { ROUTER_OUTLET_DATA } from '@angular/router';
 @Component({
@@ -7,9 +7,13 @@ import { ROUTER_OUTLET_DATA } from '@angular/router';
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
-export class BooksComponent{
-  routerData = inject(ROUTER_OUTLET_DATA)
+export class BooksComponent implements OnInit{
 
+  
+  ngOnInit(): void {
+    fetch("/api/books?skip=10&limit=5").then(d=>d.json()).then(console.log)
+  }
+  
  
 
 }
