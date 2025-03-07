e235ea17798969a5b0aa10534996492496011dc5
ilawy <next.mohammed.amr@gmail.com>
2025-03-07 21:56:27 +0200

    basic drawer layout for dashboard

--- packages/frontend/src/app/routes/admin/books/books.component.ts
+++ packages/frontend/src/app/routes/admin/books/books.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
+import { DomPortal, Portal } from  '@angular/cdk/portal'
+import { ROUTER_OUTLET_DATA } from '@angular/router';
 @Component({
   selector: 'app-books',
   imports: [],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
-export class BooksComponent {
+export class BooksComponent{
+  routerData = inject(ROUTER_OUTLET_DATA)
+
+ 
 
 }
--- packages/frontend/src/app/routes/admin/dashboard/dashboard.component.ts
+++ packages/frontend/src/app/routes/admin/dashboard/dashboard.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, TemplateRef } from '@angular/core';
 import { DashboardItem } from '../../../interfaces/dashboard.interface';
 import { TitleCasePipe } from "@angular/common";
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationStart, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { DashboardService } from "../../../services/dashboard.service";
+import { IconsModule } from '../../../modules/icons/icons.module';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    IconsModule,
+    RouterLink,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  constructor(private dashboardService: DashboardService, private router: Router) { }
+export class DashboardComponent implements OnInit {
+  constructor(private dashboardService: DashboardService, private router: Router, private elementRef: ElementRef<HTMLElement>) { }
 
-  expanded = true
 
-  toggleSide(){
-    this.expanded = !this.expanded
+  ngOnInit(): void {
+    this.router.events
+    .pipe(filter(event=>event instanceof NavigationStart))
+    .subscribe(event=>{
+      const drawer = this.elementRef.nativeElement.querySelector("input[type=checkbox]#drawer") as HTMLInputElement | null;
+      if(!drawer)return;
+      drawer.checked = false
+    })
   }
 }
