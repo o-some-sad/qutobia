8f187e421d26d7554e53070b5dfb27f3d0afb645
ilawy <next.mohammed.amr@gmail.com>
2025-03-08 12:02:53 +0200

    add book zod schema

--- packages/frontend/src/app/routes/admin/books/books.component.ts
+++ packages/frontend/src/app/routes/admin/books/books.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { DomPortal, Portal } from  '@angular/cdk/portal'
 import { ROUTER_OUTLET_DATA } from '@angular/router';
+import { BookValidator } from 'shared';
 @Component({
   selector: 'app-books',
   imports: [],
@@ -8,10 +9,10 @@ import { ROUTER_OUTLET_DATA } from '@angular/router';
   styleUrl: './books.component.css'
 })
 export class BooksComponent implements OnInit{
-
   
   ngOnInit(): void {
-    fetch("/api/books?skip=10&limit=5").then(d=>d.json()).then(console.log)
+    fetch("/api/books?skip=0&limit=10").then(d=>d.json())
+      .then(BookValidator.array().parse)
   }
   
  
--- packages/shared/index.js
+++ packages/shared/index.js
@@ -56,3 +56,4 @@ export const userLoginValidator = z.object({
 
 export * from './cart.validator.js'
 export * from './error.validator.js'
+export * from './book.validator.js'
