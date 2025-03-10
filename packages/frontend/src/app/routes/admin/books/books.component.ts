41ec0b0dac7281413a5bc412a54c9623c19122f5
ilawy <next.mohammed.amr@gmail.com>
2025-03-10 20:56:36 +0000

    feat: add search and pagination functionality to books dashboard

--- packages/frontend/src/app/routes/admin/books/books.component.ts
+++ packages/frontend/src/app/routes/admin/books/books.component.ts
@@ -3,7 +3,7 @@ import { DomPortal, Portal } from  '@angular/cdk/portal'
 import { ActivatedRoute, Router, ROUTER_OUTLET_DATA } from '@angular/router';
 import { BookValidator } from 'shared';
 import { BookService } from '../../../services/book.service';
-import { BehaviorSubject, debounceTime, map, of, Subject, tap } from 'rxjs';
+import { BehaviorSubject, debounceTime, firstValueFrom, map, of, Subject, tap } from 'rxjs';
 import { z } from 'zod';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { HeaderService } from '../../../services/header.service';
@@ -23,28 +23,41 @@ export class BooksComponent implements OnInit, OnDestroy, AfterViewInit{
   @ViewChild('headerPortal') portal!: TemplateRef<any>;
   
   search = new BehaviorSubject("")
+  currentPage = new BehaviorSubject(1);
 
 
 
   books: z.infer<typeof BookValidator>[] = []
+  totalPages: number = 0;
   loading = false;
   error: Error | null = null;
 
   ngOnInit(): void {
-      this.loadBooks()
-      
+      this.loading = true      
       this.search.next(this.route.snapshot.queryParams["search"])
+
+      this.currentPage.subscribe(async page=>{
+        console.log({page});
+        
+        this.router.navigate([], {
+          queryParamsHandling: "merge",
+          queryParams: {
+            page: page,
+          }
+        })
+      })
 
       this.search
       .pipe(debounceTime(1000))
-      .subscribe(value=>{
-        console.log({value});
+      .subscribe(async value=>{
+        this.currentPage.next(1)
         this.router.navigate([], {
           queryParamsHandling: "merge",
           queryParams: {
             search: value
           }
         })
+        await this.loadBooks()
       })
   }
 
@@ -58,12 +71,13 @@ export class BooksComponent implements OnInit, OnDestroy, AfterViewInit{
 
   
 
-  loadBooks(){
+  async loadBooks(){
     this.loading = true
-    this.booksService.getBooks(2)
+    this.booksService.getBooks(await firstValueFrom(this.currentPage), 10, await firstValueFrom(this.search))
       .subscribe({
-        next: data=>{
-          this.books=data
+        next: result=>{
+          this.totalPages = result.totalPages          
+          this.books=result.data
           this.loading = false
         },
         error: error=>{
@@ -75,6 +89,6 @@ export class BooksComponent implements OnInit, OnDestroy, AfterViewInit{
   
  
   _util_ = {
-    range: (length: number)=>Array.from({ length })
+    range: (length: number)=>Array.from({ length }, (_, i)=>i)
   }
 }
