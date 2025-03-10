ee6910b02c3d42be2706c48d23bb854415a12374
A7med zaki <ahmedzaki@gmail.com>
2025-03-11 00:36:22 +0200

    feat: add book, update book, delete book

--- packages/frontend/src/app/routes/admin/books/books.component.ts
+++ packages/frontend/src/app/routes/admin/books/books.component.ts
@@ -1,94 +1,137 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, OnDestroy, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
-import { DomPortal, Portal } from  '@angular/cdk/portal'
-import { ActivatedRoute, Router, ROUTER_OUTLET_DATA } from '@angular/router';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { BookValidator } from 'shared';
 import { BookService } from '../../../services/book.service';
-import { BehaviorSubject, debounceTime, firstValueFrom, map, of, Subject, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  debounceTime,
+  firstValueFrom,
+} from 'rxjs';
 import { z } from 'zod';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { HeaderService } from '../../../services/header.service';
 import { FormsModule } from '@angular/forms';
+import { BookDialogComponent } from '../../../components/book-dialog/book-dialog.component';
+import { BookItem } from '../../../interfaces/book.interface';
+import { toast } from 'ngx-sonner';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-books',
-  imports: [CurrencyPipe, FormsModule, CommonModule, FormsModule],
+  imports: [
+    CurrencyPipe,
+    FormsModule,
+    CommonModule,
+    FormsModule,
+    BookDialogComponent,
+  ],
   templateUrl: './books.component.html',
-  styleUrl: './books.component.css'
+  styleUrl: './books.component.css',
 })
-export class BooksComponent implements OnInit, OnDestroy, AfterViewInit{
-  private booksService = inject(BookService)
-  private headerService = inject(HeaderService)
-  private router = inject(Router)
-  private route = inject(ActivatedRoute)
+export class BooksComponent implements OnInit, OnDestroy, AfterViewInit {
+  private booksService = inject(BookService);
+  private headerService = inject(HeaderService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   @ViewChild('headerPortal') portal!: TemplateRef<any>;
-  
-  search = new BehaviorSubject("")
+  @ViewChild(BookDialogComponent) bookFormDialog!: BookDialogComponent;
+  openBookForm(mode: 'add' | 'edit', book: BookItem | null = null) {
+    if (mode === 'edit') this.bookFormDialog.openModal(mode, book);
+    else this.bookFormDialog.openModal(mode, null);
+  }
+
+  search = new BehaviorSubject('');
   currentPage = new BehaviorSubject(1);
 
-
-
-  books: z.infer<typeof BookValidator>[] = []
+  books: z.infer<typeof BookValidator>[] = [];
   totalPages: number = 0;
   loading = false;
   error: Error | null = null;
 
-  ngOnInit(): void {
-      this.loading = true      
-      this.search.next(this.route.snapshot.queryParams["search"])
-
-      this.currentPage.subscribe(async page=>{
-        console.log({page});
-        
-        this.router.navigate([], {
-          queryParamsHandling: "merge",
-          queryParams: {
-            page: page,
-          }
-        })
-      })
-
-      this.search
-      .pipe(debounceTime(1000))
-      .subscribe(async value=>{
-        this.currentPage.next(1)
-        this.router.navigate([], {
-          queryParamsHandling: "merge",
-          queryParams: {
-            search: value
-          }
-        })
-        await this.loadBooks()
-      })
+  onBookAdded(): void {
+    this.currentPage.next(1);
+    this.loadBooks();
   }
-
+  onBookUpdated(): void {
+    this.loadBooks();
+  }  
   ngAfterViewInit(): void {
     this.headerService.setPortal(this.portal);
   }
-
   ngOnDestroy(): void {
-      this.headerService.setPortal(null)
+    this.headerService.setPortal(null);
   }
 
-  
+  ngOnInit(): void {
+    this.loading = true;
+    this.search.next(this.route.snapshot.queryParams['search']);
 
-  async loadBooks(){
-    this.loading = true
-    this.booksService.getBooks(await firstValueFrom(this.currentPage), 10, await firstValueFrom(this.search))
+    this.currentPage.subscribe(async (page) => {
+      console.log({ page });
+
+      this.router.navigate([], {
+        queryParamsHandling: 'merge',
+        queryParams: {
+          page: page,
+        },
+      });
+    });
+
+    this.search.pipe(debounceTime(1000)).subscribe(async (value) => {
+      this.currentPage.next(1);
+      this.router.navigate([], {
+        queryParamsHandling: 'merge',
+        queryParams: {
+          search: value,
+        },
+      });
+      await this.loadBooks();
+    });
+  }
+
+  async loadBooks() {
+    this.loading = true;
+    this.booksService
+      .getBooks(
+        await firstValueFrom(this.currentPage),
+        10,
+        await firstValueFrom(this.search)
+      )
       .subscribe({
-        next: result=>{
-          this.totalPages = result.totalPages          
-          this.books=result.data
-          this.loading = false
+        next: (result) => {
+          this.totalPages = result.totalPages;
+          this.books = result.data;
+          this.loading = false;
         },
-        error: error=>{
-          this.error = error
-          this.loading = false
+        error: (error) => {
+          this.error = error;
+          this.loading = false;
         },
-      })
+      });
   }
-  
- 
+
+  deleteBook(bookId: string){
+    const toast_id = toast.loading('Deleting book...');
+    this.booksService.deleteBook(bookId).subscribe({
+      next: (res) => {
+        toast.success('Book deleted successfully', { id: toast_id });
+        this.loadBooks();
+      },
+      error: (_) => {
+        toast.error('Failed to delete book', { id: toast_id });
+      },
+    })
+  }
+
   _util_ = {
-    range: (length: number)=>Array.from({ length }, (_, i)=>i)
-  }
+    range: (length: number) => Array.from({ length }, (_, i) => i),
+  };
 }
