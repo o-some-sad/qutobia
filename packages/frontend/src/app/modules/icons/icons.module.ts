8b62724d1cad9db78d9f6e90e9ca53066cf4b998
ilawy <next.mohammed.amr@gmail.com>
2025-03-11 04:17:11 +0200

    add layouts for user and dashboard routes

--- packages/frontend/src/app/modules/icons/icons.module.ts
+++ packages/frontend/src/app/modules/icons/icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { Camera, Heart, Github, Monitor, Sun, Moon, User, Book, BarChart, ArrowRight, Menu, Home } from 'angular-feather/icons';
+import { Camera, Heart, Github, Monitor, Sun, Moon, User, Book, BarChart, ArrowRight, Menu, Home, Bell } from 'angular-feather/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
@@ -16,7 +16,8 @@ const icons = {
   Book,
   BarChart,
   ArrowRight,
-  Menu
+  Menu,
+  Bell
 };
 
 @NgModule({
