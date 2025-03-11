a1295ce0ccd451be27dfe3cd94c48dc75b8c1bb9
ilawy <next.mohammed.amr@gmail.com>
2025-03-11 23:52:50 +0200

    test: add production environment

--- packages/frontend/src/app/services/dashboard.service.ts
+++ packages/frontend/src/app/services/dashboard.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map, Observable} from 'rxjs';
 import {DashboardItem, DashboardResponse} from '../interfaces/dashboard.interface';
-import {environment} from '../../environments/environment.development';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
