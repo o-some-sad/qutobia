e50cfeb49f092a92ecac608ec6ae47dcfe3ec403
ilawy <next.mohammed.amr@gmail.com>
2025-03-11 04:23:44 +0200

    feat: theming is now a separated service

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { NgxSonnerToaster } from 'ngx-sonner';
 import { CartService } from './services/cart.service';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { ThemingService } from './services/theming.service';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,6 @@ import { HttpClient, provideHttpClient } from '@angular/common/http';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  themeing = inject(ThemingService)
   title = 'frontend';
 }
