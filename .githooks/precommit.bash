8ac79a741f9b1dde7c30b9d2d2be51da619af425
ilawy <next.mohammed.amr@gmail.com>
2025-03-11 02:03:47 +0200

    fix: move pre-commit logic to be run using bash

--- .githooks/precommit.bash
+++ .githooks/precommit.bash
@@ -1,11 +1,11 @@
 p=`pwd`
-# {
-#     cd packages/backend
-#     pnpm eslint . --fix
-# };
-# if [[ "$?" -ne 0 ]]; then
-#     exit 1
-# fi
+{
+    cd packages/backend
+    pnpm eslint . --fix
+};
+if [[ "$?" -ne 0 ]]; then
+    exit 1
+fi
 cd $p
 {
     cd packages/frontend
