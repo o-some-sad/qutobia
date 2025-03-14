4cf32adac4c30f543f58bb143ac39902a0efc94e
ilawy <next.mohammed.amr@gmail.com>
2025-03-14 02:28:13 +0000

    fix: redirect payment to right proper places

--- packages/backend/routes/payment.route.js
+++ packages/backend/routes/payment.route.js
@@ -4,6 +4,7 @@ import express from "express";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 import ApiError from "../utilities/apiError.js";
 import * as PaymentController from "../controllers/payment.controller.js";
+import Payment from "../models/payment.model.js";
 
 export const paymentRouter = express.Router();
 
@@ -26,20 +27,27 @@ paymentRouter.get("/success", authenticateToken, async (req, res, next) => {
   try {
     const { payment } = req.query;
     //TODO: THIS SHOULD NEVER HAPPEN
-    if (!payment || typeof payment !== "string"){
+    if (!payment || typeof payment !== "string") {
       throw new ApiError("Cannot retrive payment reference", 400);
     }
     await PaymentController.transformPayment(payment, req.user._id);
-    return res.redirect("/");
+    return res.redirect("/order");
   } catch (err) {
-    console.log("WA?T", err);
-    
     return next(err);
   }
 });
 
-paymentRouter.get("/cancel", (req, res) => {
-  console.log("CANCEL", req.query);
+paymentRouter.get("/cancel", async (req, res, next) => {
+  try {
+    const { payment } = req.query;
+    //TODO: THIS SHOULD NEVER HAPPEN
+    if (!payment || typeof payment !== "string") {
+      throw new ApiError("Cannot retrive payment reference", 400);
+    }
+    await Payment.deleteOne({ _id: payment })
 
-  res.redirect("/order");
+    res.redirect("/");
+  } catch (e) {
+    next(e)
+  }
 });
